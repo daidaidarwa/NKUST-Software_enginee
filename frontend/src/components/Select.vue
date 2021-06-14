a8aff74e69bd3767403a7072c42e4648dfd8187a
daidaidarwa <open159632@gmail.com>
2021-06-15 07:48:43 +0800

    修正縮排

--- frontend/src/components/Select.vue
+++ frontend/src/components/Select.vue
@@ -1,35 +1,33 @@
 <template>
     <div class="d-flex justify-center mt-10" >
     <v-card dark>
-    <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-        <v-tab v-for="item in items" :key="item">
-        {{ item }}
-        </v-tab>
-    </v-tabs> 
-    <v-tabs-items v-model="tab">
-        <v-tab-item v-for="item in items" :key="item">
-        <v-card color="basil" flat>
-            <v-card v-if="item==='首頁'">
-                <Ad />
-            </v-card>
-            <v-card v-else>
-            <v-card elevation="24" width="1000" class="mx-auto">
-                <v-row>
-                <v-col v-for="n in 6" :key="n" class="d-flex child-flex" cols="4">
-                    <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`" :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" class="grey lighten-2">
-                    <template v-slot:placeholder>
-                        <v-row class="fill-height ma-0" align="center" justify="center">
-                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+            <v-tab v-for="item in items" :key="item">
+                {{ item }}
+            </v-tab>
+        </v-tabs> 
+        <v-tabs-items v-model="tab">
+            <v-tab-item v-for="item in items" :key="item">
+                <v-card color="basil" flat>
+                    <v-card v-if="item==='首頁'">
+                        <Ad />
+                    </v-card>
+                    <v-card v-else elevation="24" width="1000" class="mx-auto">
+                        <v-row>
+                            <v-col v-for="n in 6" :key="n" class="d-flex child-flex" cols="4">
+                                <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`" :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" class="grey lighten-2">
+                                    <template v-slot:placeholder>
+                                        <v-row class="fill-height ma-0" align="center" justify="center">
+                                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                        </v-row>
+                                    </template>
+                                </v-img>
+                            </v-col>
                         </v-row>
-                    </template>
-                    </v-img>
-                </v-col>
-                </v-row>
-            </v-card>
-            </v-card>
-        </v-card>
-        </v-tab-item>
-    </v-tabs-items>
+                    </v-card>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
     </v-card>
 </div>    
 </template>
